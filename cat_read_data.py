b54c5661cfc7b953559f3388103887a4ce84a136
rem <remch183@outlook.com>
2017-02-06 13:57:37 +0800

    Apply simplified VGG model.

--- cat_read_data.py
+++ cat_read_data.py
@@ -1,51 +1,94 @@
 import numpy as np
+import random
 import os
 from PIL import Image
 from sklearn.cross_validation import train_test_split
 import pickle
 
 
-def get_train(shuffle=True, seed=None):
+def get_files_path(shuffle=True, seed=None):
+    if seed:
+        random.seed(seed)
+    root_path = 'input/my_train/'
+    files_path = [root_path + i for i in os.listdir(root_path)]
+    if shuffle:
+        random.shuffle(files_path)
+    return files_path
+
+
+def get_train_val_generator(validation_index=None, val_split=0.15, batchsize=32):
+    files = get_files_path()
+    if validation_index is None:
+        validation_index = int(float(len(files)) * val_split)
+
+    def train():
+        while True:
+            gen = get_xy_data(files[validation_index:], True, batchsize*10)
+            for X_train, y_train in gen:
+                for idx in range(0, len(X_train), batchsize):
+                    next_X_train = X_train[idx:idx+batchsize]
+                    next_y_train = y_train[idx:idx+batchsize]
+                    next_X_train = next_X_train.astype(np.float16)
+                    next_X_train = (next_X_train - 128.) / 128.
+                    yield next_X_train, next_y_train
+
+    def val():
+        while True:
+            gen = get_xy_data(files[:validation_index], False, batchsize*10)
+            for X_train, y_train in gen:
+                for idx in range(0, len(X_train), batchsize):
+                    next_X_train = X_train[idx:idx+batchsize]
+                    next_y_train = y_train[idx:idx+batchsize]
+                    next_X_train = next_X_train.astype(np.float16)
+                    next_X_train = (next_X_train - 128.) / 128.
+                    yield next_X_train, next_y_train
+    return train, val
+
+
+def get_xy_data(files_path, train=True, batchsize=320):
     """return X_train, Y_train"""
 
-    data_save = 'train_data.pickle'
-    if os.path.exists(data_save):
-        with open(data_save, 'rb') as f:
-            X_train, Y_train = pickle.load(f)
-        return X_train, Y_train
-    root_path = 'input/my_train/'
-    files_path = [ root_path + i for i in os.listdir(root_path)]
+    train_pickle = 'data_pickle/'
+    if any((str(batchsize) in i for i in os.listdir(train_pickle))):
+        for picklename in os.listdir(train_pickle):
+            if (train and 'train' in picklename) or (not train and 'val' in picklename):
+                with open(train_pickle + picklename, 'rb') as f:
+                    yield pickle.load(f)
 
     def get_label(name):
         if 'cat' in name:
             return [0, 1]
         return [1, 0]
-    Y_train = np.array(list(map(get_label, files_path)))
-    X_train = np.array(list(map(lambda x: np.array(Image.open(x)), files_path)))
 
-    mask = np.ones(X_train.shape[0]).astype(np.bool)
-    for i,v in enumerate(X_train):
-        if v.shape != (128, 128, 3):
-            mask[i] = False
-    X_train = np.array(X_train[mask])
-    Y_train = Y_train[mask]
-    X_train = np.array(X_train.tolist())
-    if shuffle:
-        if not seed:
-            X_train, _, Y_train, _ = train_test_split(X_train, Y_train, test_size=0.0)
-        else:
-            X_train, _, Y_train, _ = train_test_split(X_train, Y_train, test_size=0.0, random_state=seed)
-    with open(data_save, 'wb') as f:
-        pickle.dump([X_train, Y_train], f)
-    return X_train, Y_train
+    counter = 0
+    if train:
+        this_type = 'train'
+    else:
+        this_type = 'val'
+    for i in range(0, len(files_path), batchsize):
+        counter += 1
+        X_train = []
+        Y_train = []
+        for j in range(i, min(i+batchsize, len(files_path))):
+            new_img = Image.open(files_path[j])
+            if new_img.size != (256, 256):
+                continue
+            X_train.append(np.array(new_img))
+            Y_train.append(get_label(files_path[j]))
+        X_train = np.array(X_train)
+        Y_train = np.array(Y_train)
+        yield X_train, Y_train
+        with open(train_pickle + this_type + '%d_%d.pickle' % (batchsize, counter), 'wb') as f:
+            pickle.dump((X_train, Y_train), f)
+        del X_train, Y_train
 
 
 def get_test():
     """return X_test"""
     root_path = 'input/my_test/'
-    files_path = [ root_path + i for i in os.listdir(root_path)]
+    files_path = [root_path + i for i in os.listdir(root_path)]
     X_test = np.array(list(map(lambda x: np.array(Image.open(x)), files_path)))
-    for i,v in enumerate(X_test):
+    for i, v in enumerate(X_test):
         if v.shape != (128, 128, 3):
             raise ValueError("Shape not equal to (128, 128, 3) Error!!!")
     return X_test
--- model.py
+++ model.py
@@ -13,24 +13,25 @@ import keras
 import re
 import numpy as np
 import os
-from cat_read_data import get_train, get_test
+from cat_read_data import get_test, get_train_val_generator
 
-def get_model():
+
+def get_model_v0():
     model = Sequential()
     # input: 100x100 images with 3 channels -> (3, 100, 100) tensors.
     # this applies 32 convolution filters of size 3x3 each.
-    model.add(Convolution2D(32, 3, 3, border_mode='valid', input_shape=(128, 128, 3)))
+    model.add(Convolution2D(32, 3, 3, border_mode='valid', input_shape=(256, 256, 3)))
     model.add(Activation('relu'))
     model.add(Convolution2D(32, 3, 3))
     model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(4, 4)))
     model.add(Dropout(0.25))
 
     model.add(Convolution2D(64, 3, 3, border_mode='valid'))
     model.add(Activation('relu'))
     model.add(Convolution2D(64, 3, 3))
     model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(4, 4)))
     model.add(Dropout(0.25))
 
     model.add(Flatten())
@@ -48,67 +49,122 @@ def get_model():
     return model
 
 
+def get_model_v1():
+    model = Sequential()
+    # input: 100x100 images with 3 channels -> (3, 100, 100) tensors.
+    # this applies 32 convolution filters of size 3x3 each.
+    model.add(Convolution2D(32, 3, 3, border_mode='valid', input_shape=(256, 256, 3)))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(32, 3, 3))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(64, 3, 3, border_mode='valid'))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(64, 3, 3))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(128, 3, 3, border_mode='valid'))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(128, 3, 3))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(64, 1, 1, border_mode='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    # model.add(Convolution2D(256, 3, 3, border_mode='valid'))
+    # model.add(Activation('relu'))
+    # model.add(Convolution2D(256, 3, 3))
+    # model.add(Activation('relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    # Note: Keras does automatic shape inference.
+    model.add(Dense(256))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(256))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(2))
+    model.add(Activation('softmax'))
+
+    # sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+    # adamax = keras.optimizers.Adamax(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+    optimizer = keras.optimizers.RMSprop(lr=1e-4)
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy', 'categorical_crossentropy'])
+
+    return model
+
+
 def standerize_images(X):
     X = X.astype(np.float16)
     X = (X - 128.) / 256.
     return X
 
 
-def train(model, start=0):
-    batch_size = 32
-    validation_index = 1200
-    save_step = 20
-    print('Getting Training Data.')
-    X_train, Y_train = get_train()
-
-    def generator(X_input, Y_input):
-        while True:
-            for idx in range(0, len(X_input), batch_size):
-                X = X_input[idx: min(len(X_input)-1, idx + batch_size)]
-                Y = Y_input[idx: min(len(X_input)-1, idx + batch_size)]
-                X = standerize_images(X)
-                yield X, Y
-    print('\rGot Training Data.')
+def train(model, start=0, version=''):
+    batch_size = 16
+    validation_index = batch_size*10 * (2000 // (batch_size * 10))
+    save_step = 5
+    length = 25000
+    train_gen, val_gen = get_train_val_generator(validation_index=validation_index, batchsize=batch_size)
 
     def scheduler(epoch):
-        if epoch >= 50:
-            return 0.0002
-        return 0.002
-    callback = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=1, write_graph=True, write_images=False)
+        if epoch >= 39:
+            return 1e-5
+        return 1e-4
+
+    callback = keras.callbacks.TensorBoard(log_dir='./logs'+version, histogram_freq=1, write_graph=True, write_images=False)
     change_lr = keras.callbacks.LearningRateScheduler(scheduler)
 
-    for i in range(start, start+20, save_step):
-        model.fit_generator(generator=generator(X_train[validation_index:], Y_train[validation_index:]),
-                            samples_per_epoch=(len(X_train) - validation_index)//batch_size,
+    for i in range(start, start+save_step*20, save_step):
+        model.fit_generator(generator=train_gen(),
+                            samples_per_epoch=(length - validation_index),
                             nb_epoch=i+save_step,
                             initial_epoch=i,
-                            max_q_size=2,
                             callbacks=[callback, change_lr],
-                            validation_data=generator(X_train[:validation_index], Y_train[:validation_index]),
-                            nb_val_samples=validation_index // batch_size,)
-        model.save('models/trained_model_%05d.model' % (i+save_step, ))
+                            # callbacks=[callback, ],
+                            validation_data=val_gen(),
+                            nb_val_samples=validation_index )
+        model.save('models_' + version + '/trained_model_%05d.model' % (i+save_step, ))
     return model
 
 
-def main(load=True):
-    print('Model loaded.')
-    models_path = os.listdir('models')
+def main(load=True, version=''):
+    models_root = 'models_' + version + '/'
+    models_path = os.listdir(models_root)
     if load and models_path:
-        models_path = 'models/' + models_path[-1]
+        models_path = models_root + models_path[-1]
+        print('\n\n\n========================')
         print('Loading model from file: ' + models_path)
+        start = re.findall(r'\d+', models_path)[-1]
+        start = int(start.lstrip('0'))
+        print('Start from epoch: ' + str(start))
+        print('========================\n\n\n')
         model = load_model(models_path)
-        start = re.findall(r'\d+', models_path)[0]
-        start = int(start)
     else:
-        model = get_model()
+        model = get_model_v1()
         start = 0
-    model = train(model, start)
+    print('Model loaded.')
+    print("Parameters num: " + str(model.count_params()))
+    print(model.summary())
+    model = train(model, start, version=version)
     print('Finish!')
     del model
     return
 
 if __name__ == '__main__':
-    main()
+    main(version='v1')
 
 
 
